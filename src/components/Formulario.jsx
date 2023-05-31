56b510801be30bd12fc7aa6e5ac2cd8aac9e01d7
Ana Santillan <anisantillan10@gmail.com>
2023-05-31 00:32:25 -0300

    se agrego una parte de la logica del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,21 @@
 import { Button, Form } from "react-bootstrap";
 import Lista from "./Lista";
+import { useState } from "react";
 
 const Formulario = () => {
+    const [tarea, setTarea] = useState("");
+    const [tareas, setTareas] = useState([]);
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setTareas([...tareas, tarea])
+    }
     return (
         <>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3 d-flex" controlId="tarea">
-                <Form.Control type="text" placeholder="Ingrese una tarea" />
-                <Button variant="primary">Agregar</Button>
+                <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
+                <Button variant="primary" type="submit">Agregar</Button>
             </Form.Group>
         </Form>
         <Lista></Lista>
