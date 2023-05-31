259b83d0e1ec2cc5e174d633317656214c46451b
Ana Santillan <anisantillan10@gmail.com>
2023-05-30 23:10:43 -0300

    se agrego el footer y se utilizaron clases de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Container } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      
+      <Container className='my-5 main'>
+        <h1 className='display-4 text-center'>Lista de tareas</h1>
+        <hr />
+      </Container>
+      <footer className='text-center bg-dark py-4 text-light'>
+        <p> &copy; Todos los derechos reservados </p>
+      </footer>
     </>
   )
 }
